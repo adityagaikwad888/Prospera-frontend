a1d7588b37e4bd9dbef2e3955e48b41e326c0a25
adityagaikwad888 <adityakg99@gmail.com>
2025-04-09 11:35:12 +0530

    day 6 p 1

--- src/const_var.js
+++ src/const_var.js
@@ -1,2 +1,2 @@
 // export const VITE_BACKEND_URL = "http://localhost:3001"; // Replace with your backend URL
-export const VITE_BACKEND_URL = "http://65.1.106.80:3001";
+export const VITE_BACKEND_URL = "https://65.1.106.80/api";
